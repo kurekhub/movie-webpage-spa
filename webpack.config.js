3615b4c953e0c87ed5376c75b5b3af4db4e0bf6f
Hubert Kurek <hkurkov@gmail.com>
2017-05-12 19:35:26 +0200

    more config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 let { CheckerPlugin, TsConfigPathsPlugin } = require("awesome-typescript-loader");
 let path = require("path");
 
-let entryFilePath = "./src/index.ts"
-let buildPath = path.resolve(__dirname, "build") // __dirname == W:\
+let entryFilePath = path.resolve(__dirname, "src/main.ts");
+let vendorPath = path.resolve(__dirname, "src/vendor.ts");
+
+let buildPath = path.resolve(__dirname, "build")
 let buildFilename = "build.js"
 
 let config = {
-    entry: entryFilePath,
+    entry: {
+        app: entryFilePath,
+        vendor: vendorPath
+    },
     output: {
         path: buildPath,
         filename: buildFilename
@@ -22,11 +27,11 @@ let config = {
                 loader: "awesome-typescript-loader",
                 exclude: [
                     path.resolve(__dirname, "node_modules")
-               ],
-               options: {
-                   configFileName: "./src/tsconfig.json"
-               }
-            }
+                ],
+                options: {
+                    configFileName: "./src/tsconfig.json"
+                }
+            },
         ]
     },
     plugins: [
